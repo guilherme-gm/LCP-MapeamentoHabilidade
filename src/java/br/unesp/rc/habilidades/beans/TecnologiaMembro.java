deceb43b3d245ea15c1e5460258bbb87b5dd60ed
raphaelTrench <raphaeltrench@gmail.com>
2017-06-23 09:38:58 -0600

    TecnologiaMembroDAO

--- src/java/br/unesp/rc/habilidades/beans/TecnologiaMembro.java
+++ src/java/br/unesp/rc/habilidades/beans/TecnologiaMembro.java
@@ -7,9 +7,44 @@ public class TecnologiaMembro {
 	private short nivel;
 
 	private Tecnologia tecnologia;
+        
+        private Membro membro;
 
 	public void validate() {
 
-	}
+        }
+
+    public long getIdTecnologiaMembro() {
+        return idTecnologiaMembro;
+    }
+
+    public void setIdTecnologiaMembro(long idTecnologiaMembro) {
+        this.idTecnologiaMembro = idTecnologiaMembro;
+    }
+
+    public short getNivel() {
+        return nivel;
+    }
+
+    public Membro getMembro() {
+        return membro;
+    }
+
+    public void setMembro(Membro membro) {
+        this.membro = membro;
+    }
+
+    public void setNivel(short nivel) {
+        this.nivel = nivel;
+    }
+
+    public Tecnologia getTecnologia() {
+        return tecnologia;
+    }
+
+    public void setTecnologia(Tecnologia tecnologia) {
+        this.tecnologia = tecnologia;
+    }
+        
 
 }
