e9677cccf9e596cc7fc35302dfb231f55b5248ca
raphael <raphaeltrench@gmail.com>
2017-07-06 20:08:06 -0300

    validar Tecnologia e Projeto

--- src/java/br/unesp/rc/habilidades/commands/DoCriarProjeto.java
+++ src/java/br/unesp/rc/habilidades/commands/DoCriarProjeto.java
@@ -8,6 +8,7 @@ package br.unesp.rc.habilidades.commands;
 import br.unesp.rc.habilidades.beans.Permissao;
 import br.unesp.rc.habilidades.beans.Projeto;
 import br.unesp.rc.habilidades.beans.StatusProjeto;
+import br.unesp.rc.habilidades.exception.ValidateException;
 import br.unesp.rc.habilidades.util.PermissaoUtils;
 import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
@@ -37,6 +38,22 @@ public class DoCriarProjeto implements ICommand {
             Logger.getLogger(DoCriarProjeto.class.getName()).log(Level.SEVERE, null, ex);
         } catch (InvocationTargetException ex) {
             Logger.getLogger(DoCriarProjeto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            projeto.validate();
+        } catch (ValidateException ex){
+            StringBuilder sb = new StringBuilder();
+            sb.append("Erros na inserção: <br /><ul>");
+            for (String erro : ex.getErros()) {
+                sb.append("<li>").append(erro).append("</li>");
+            }
+            sb.append("</ul>");
+
+            request.setAttribute("msg_tipo", "alert-danger");
+            request.setAttribute("msg", sb.toString());
+            request.setAttribute("menu", "adminproj");
+            return new CommandResult(request, "CriarProjetos");
         }
         
         HttpSession session = request.getSession(true);
